ed2b612d37ab74de629c921afa8655ecd4da440e
DevJARM <repo.ruiz.javier@gmail.com>
2022-10-10 20:38:27 -0500

    fix: manipulando filtro PRE

--- api-gateway/src/main/java/com/udemy/apigateway/Filter/GlobalFilter.java
+++ api-gateway/src/main/java/com/udemy/apigateway/Filter/GlobalFilter.java
@@ -3,28 +3,42 @@ package com.udemy.apigateway.Filter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.Ordered;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.Optional;
+
 @Component
-public class GlobalFilter implements org.springframework.cloud.gateway.filter.GlobalFilter {
+public class GlobalFilter implements org.springframework.cloud.gateway.filter.GlobalFilter, Ordered {
 
     private final Logger log = LoggerFactory.getLogger(GlobalFilter.class);
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 
-        log.info("EJECUTO EL FILTRO PRE");
+        log.info("STEP 1 - EL FILTRO PRE");
+
+        exchange.getRequest().mutate().headers(h -> h.add("token", "123456"));
 
         return chain.filter(exchange).then(Mono.fromRunnable(() -> {
 
-            log.info("EJECUTO FILTRO POST");
+            log.info("STEP 2 - FILTRO POST");
 
-            exchange.getResponse().getCookies().add("COLOR", ResponseCookie.from("COLOR", "ROJO").build());
+            Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token")).ifPresent(valor -> {
+                exchange.getResponse().getHeaders().add("token", valor);
+            });
+
+            exchange.getResponse().getCookies().add("COLOR", ResponseCookie.from("COLOR", "VERDE").build());
             exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
         }));
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
     }
 }
