35343d310e1ce3bf7ab75110d82db0bbba2dff8a
DevJARM <repo.ruiz.javier@gmail.com>
2022-11-18 15:57:53 -0500

    ref: cambiando el jwt secert key

--- user-service/src/main/resources/import.sql
+++ user-service/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO userT (username, password, enabled, name, last_name, email) VALUES ('jruiz', '$2a$10$Q4eplUvrS/sT27zSQi4Ti.wkDvAvFjiQtEMzjEA9TPdfWxs0zHkPa', 1, 'Javier', 'Ruiz', 'jruiz@email.com');
-INSERT INTO userT (username, password, enabled, name, last_name, email) VALUES ('pgonzales', '$2a$10$Q4eplUvrS/sT27zSQi4Ti.wkDvAvFjiQtEMzjEA9TPdfWxs0zHkPa', 1, 'Patricia', 'Gonzales', 'pgonzales@email.com');
+INSERT INTO userT (username, password, enabled, name, last_name, email) VALUES ('jruiz', '$2a$10$Gea/WOVstBKBu95jQl0Q0uzUYBJmonqi8/NLYQeeJG6l3aVEydx0a', 1, 'Javier', 'Ruiz', 'jruiz@email.com');
+INSERT INTO userT (username, password, enabled, name, last_name, email) VALUES ('pgonzales', '$2a$10$Gea/WOVstBKBu95jQl0Q0uzUYBJmonqi8/NLYQeeJG6l3aVEydx0a', 1, 'Patricia', 'Gonzales', 'pgonzales@email.com');
 
 INSERT INTO roleT (role_name) VALUES ('ROLE_ADMIN');
 INSERT INTO roleT (role_name) VALUES ('ROLE_USER');
