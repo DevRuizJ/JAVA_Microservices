8189112cfe44456221a324b13a3d7b90721b2b1f
DevJARM <repo.ruiz.javier@gmail.com>
2022-11-23 21:03:39 -0500

    feat: se añade coneccion a db MYSQL

--- user-service/src/main/resources/import.sql
+++ user-service/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO userT (username, password, enabled, name, last_name, email) VALUES ('jruiz', '$2a$10$Gea/WOVstBKBu95jQl0Q0uzUYBJmonqi8/NLYQeeJG6l3aVEydx0a', 1, 'Javier', 'Ruiz', 'jruiz@email.com');
-INSERT INTO userT (username, password, enabled, name, last_name, email) VALUES ('pgonzales', '$2a$10$Gea/WOVstBKBu95jQl0Q0uzUYBJmonqi8/NLYQeeJG6l3aVEydx0a', 1, 'Patricia', 'Gonzales', 'pgonzales@email.com');
+INSERT INTO userT (username, password, enabled, name, last_name, email) VALUES ('jruiz', '$2a$10$AUKALWJiGWzedaOWN4owJex8CZmhl9VHoSkOhn387Q4HheFIrK2lu', 1, 'Javier', 'Ruiz', 'jruiz@email.com');
+INSERT INTO userT (username, password, enabled, name, last_name, email) VALUES ('pgonzales', '$2a$10$drk79q1CTYl0db.MXQgbV.J3eFaToh9rNt1766/UyuzNJoCA3q2PW', 1, 'Patricia', 'Gonzales', 'pgonzales@email.com');
 
 INSERT INTO roleT (role_name) VALUES ('ROLE_ADMIN');
 INSERT INTO roleT (role_name) VALUES ('ROLE_USER');
