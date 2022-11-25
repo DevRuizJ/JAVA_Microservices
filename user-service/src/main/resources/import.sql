96afc48c6343da10879bad33f3b30338f70e3e5b
DevJARM <repo.ruiz.javier@gmail.com>
2022-11-25 15:47:42 -0500

    feat: se agrega coneccion a config server desde servicio usuarios

--- user-service/src/main/resources/import.sql
+++ user-service/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO userT (username, password, enabled, name, last_name, email) VALUES ('jruiz', '$2a$10$AUKALWJiGWzedaOWN4owJex8CZmhl9VHoSkOhn387Q4HheFIrK2lu', 1, 'Javier', 'Ruiz', 'jruiz@email.com');
-INSERT INTO userT (username, password, enabled, name, last_name, email) VALUES ('pgonzales', '$2a$10$drk79q1CTYl0db.MXQgbV.J3eFaToh9rNt1766/UyuzNJoCA3q2PW', 1, 'Patricia', 'Gonzales', 'pgonzales@email.com');
+INSERT INTO userT (username, password, enabled, name, last_name, email) VALUES ('jruiz', '$2a$10$AUKALWJiGWzedaOWN4owJex8CZmhl9VHoSkOhn387Q4HheFIrK2lu', true, 'Javier', 'Ruiz', 'jruiz@email.com');
+INSERT INTO userT (username, password, enabled, name, last_name, email) VALUES ('pgonzales', '$2a$10$drk79q1CTYl0db.MXQgbV.J3eFaToh9rNt1766/UyuzNJoCA3q2PW', true, 'Patricia', 'Gonzales', 'pgonzales@email.com');
 
 INSERT INTO roleT (role_name) VALUES ('ROLE_ADMIN');
 INSERT INTO roleT (role_name) VALUES ('ROLE_USER');
